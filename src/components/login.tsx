659dd9f18132fa3c330470fc9ee9ef006a8e5ee3
mallikaravi <mailtomallikaravi@gmail.com>
2022-09-30 15:00:43 +0200

    written code for login page

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,44 +1,26 @@
+import ReactDOM from "react-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+import "./styles/index.css";
+
+interface IFormInput {
+  Email: string;
+  Password: string;
+}
+
 export default function Login() {
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit = (data: IFormInput) => console.log(data);
+
   return (
-    <form>
-      <h3>Sign In</h3>
-      <div className="mb-3">
-        <label>Email address</label>
-        <input
-          type="Username"
-          className="form-control"
-          placeholder="Enter username"
-        />
-        <span className="fa fa-user"></span>
-      </div>
-      <div className="mb-3">
-        <label>Password</label>
-        <input
-          type="password"
-          className="form-control"
-          placeholder="Enter password"
-        />
-      </div>
-      <div className="mb-3">
-        <div className="custom-control custom-checkbox">
-          <input
-            type="checkbox"
-            className="custom-control-input"
-            id="customCheck1"
-          />
-          <label className="custom-control-label" htmlFor="customCheck1">
-            Remember me
-          </label>
-        </div>
-      </div>
-      <div className="d-grid">
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </div>
-      <p className="forgot-password text-right">
-        Forgot <a href="#">password?</a>
-      </p>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Email</label>
+      <input {...register("Email")} placeholder="Your Email" />
+      <label>Password</label>
+      <input {...register("Password")} placeholder="Your Password" />
+
+      <input type="submit" />
     </form>
   );
 }
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Login />, rootElement);
--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,16 +1,3 @@
 import React from "react";
 
-interface iProps {
-  title: string;
-  text: string;
-  imageURL: string;
-}
-export default function WelcomePage({ title, text, imageURL }: iProps) {
-  return (
-    <article>
-      <h1></h1>
-      <title></title>
-      <img></img>
-    </article>
-  );
-}
+export function WelcomePage() {}
